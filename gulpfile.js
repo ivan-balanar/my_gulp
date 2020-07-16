afd1a0edd1484a3aae1bee9c52c586012954ce8b
ivan_bal <semenov.siberian@gmail.com>
2020-07-16 22:20:57 +0700

    add sass

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+let project_folder="dist";
+let source_folder="#src";
+
+let path={
+    build:{
+        html: project_folder+"/",
+        css: project_folder+"/css/",
+        js: project_folder+"/js/",
+        img: project_folder+"/img/",
+        fonts: project_folder+"/fonts/",
+    },
+    src:{
+        html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+        css: source_folder + "/scss/style.scss",
+        js: source_folder+"/js/script.js",
+        img: source_folder+"/img/**/*.{jpg,png,svg,gif,ico,webp}",
+        fonts: source_folder+"/fonts/*.ttf",
+    },
+    watch:{
+        html: source_folder+"/**/*.html",
+        css: source_folder+"/scss/**/*.scss",
+        js: source_folder+"/js/**/*.js",
+        img: source_folder+"/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    },
+    clean:"./" + project_folder+"/"
+}
+
+let { src, dest } = require('gulp'),
+    gulp = require('gulp'), 
+    browsersync = require("browser-sync").create(),
+    fileinclude = require("gulp-file-include"),
+    del = require("del"),
+    scss = require("gulp-sass");
+
+function browserSync(params) {
+    browsersync.init({
+        server: {
+            baseDir: "./" + project_folder+"/"
+        },
+        port:3000,
+        notify:false
+    })
+}
+
+function html() {
+    return src(path.src.html)
+        .pipe(fileinclude())
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream())
+}
+
+function css() {
+    return src(path.src.css)
+        .pipe(
+            scss({
+                outputStyle: "expanded"
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream())
+}
+
+
+
+function watchFiles(params) {
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+}
+
+function clean(params) {
+    return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(css, html));
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.css = css;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
